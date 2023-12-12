f3bb67fa370dcdf044ce08a6bcc84dc9a1175c72
Anuskota <anamangada.dev@gmail.com>
2023-12-12 22:43:57 +0100

    -init testing

---
+++ test/FizzBuzz.test.js
@@ -0,0 +1,14 @@
+// Imports
+
+const { expect } = require("@jest/globals")
+
+
+
+
+
+// Feature para saber cuando un numero es primo de 3 y de 5.
+//     1.Cuando el usuario digitalice un numero divisible por 3, devuelve Fizz,
+//     2.Cuando el usuario digitalice un numero divisible por 5, devuelve Buzz,
+//     3.Cuando el usuario digitalice un numero divisible por 3 y 5, devuelve FizzBuzz,
+//     4. Cuando el usuario digitalice un numero y no sea divisible por 3 y 5, devuelve el numero.
+
